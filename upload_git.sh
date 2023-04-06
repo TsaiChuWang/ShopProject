ee438da479b2e4aacd1b25335f345bdf965dc986
TsaiChuWang <wtsaichu@gmail.com>
2023-04-06 14:48:28 +0800

    first

--- upload_git.sh
+++ upload_git.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd ~/Documents/workspace/H-ShopProject
-git remote set-url origin https://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@github.com/TsaiChuWang/ShopProject.git
+git remote set-url origin https://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@github.com/TsaiChuWang/ShopProject.git
 git add .
 git add -u .
-git commit -m "send option cmd"
+git commit -m "first"
 git push -u origin main
-
-git remote set-url origin https://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@github.com/TsaiChuWang/ShopProject.git
